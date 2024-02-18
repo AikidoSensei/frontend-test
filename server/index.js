2e3f013476e104401e47330c7ecb33b1aca1825a
Your Name <tragger25@gmail.com>
2024-02-18 19:28:46 +0100

    fix

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const spinRoute = require('./route')
 app.use(bodyParser.json())
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://adavize-ensake-test.vercel.app/',
     methods: 'GET',
     credentials: true,
   })
